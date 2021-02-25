527bde8954483d3b6f08065d7b4f382c3d273d39
Deniffer <liuwenqing1118@qq.com>
2021-02-25 19:15:04 +0800

    first commit

---
+++ complex.h
@@ -0,0 +1,22 @@
+#ifndef __COMPLEX__    //防卫式声明 防止多次include
+#define __COMPLEX__
+
+
+//forward declarations 前置声明
+#include <cmath>
+
+class ostream
+class complex
+
+complex& 
+    __dopl(complex& ths, const complex& r);
+
+//class declarations 类声明
+class complex{ // class head
+
+}
+
+//class definitions 类定义
+complex::function
+
+#endif 
